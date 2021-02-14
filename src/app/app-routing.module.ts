2fee992bafd20bcccf2fe245bbddb5f1d3cd3800
Sean Swanson <sean-swan@hotmail.com>
2021-02-13 20:29:10 -0800

    Stubbed out components that will be used as form examples. Removed boilerplate code. Installed Material.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,37 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ArrayFormComponent } from "./array-form/array-form.component";
+import { BasicFormComponent } from "./basic-form/basic-form.component";
+import { HomeComponent } from "./home/home.component";
+import { NestedFormComponent } from "./nested-form/nested-form.component";
+import { SubmitFormComponent } from "./submit-form/submit-form.component";
+import { ValidFormComponent } from "./valid-form/valid-form.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "basic",
+    component: BasicFormComponent,
+  },
+  {
+    path: "array",
+    component: ArrayFormComponent,
+  },
+  {
+    path: "nested",
+    component: NestedFormComponent,
+  },
+  {
+    path: "submit",
+    component: SubmitFormComponent,
+  },
+  {
+    path: "valid",
+    component: ValidFormComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BasicFormComponent } from "./basic-form/basic-form.component";
+import { NestedFormComponent } from "./nested-form/nested-form.component";
+import { ArrayFormComponent } from "./array-form/array-form.component";
+import { SubmitFormComponent } from "./submit-form/submit-form.component";
+import { ValidFormComponent } from "./valid-form/valid-form.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ReactiveFormsModule } from "@angular/forms";
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicFormComponent,
+    NestedFormComponent,
+    ArrayFormComponent,
+    SubmitFormComponent,
+    ValidFormComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
