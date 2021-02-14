8f23540e1be85cab85a004feb12b23c95a4e241b
Sean Swanson <sean-swan@hotmail.com>
2021-02-13 21:36:17 -0800

    Added sidenav for traversing the form examples. Adjusted layout styling.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,14 @@ import { SubmitFormComponent } from "./submit-form/submit-form.component";
 import { ValidFormComponent } from "./valid-form/valid-form.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { ReactiveFormsModule } from "@angular/forms";
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from "./home/home.component";
+import { MatCardModule } from "@angular/material/card";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatListModule } from "@angular/material/list";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 @NgModule({
   declarations: [
@@ -27,8 +34,16 @@ import { HomeComponent } from './home/home.component';
     AppRoutingModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    FlexLayoutModule,
   ],
-  providers: [],
+  exports: [MatSidenavModule],
+  providers: [FlexLayoutModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
